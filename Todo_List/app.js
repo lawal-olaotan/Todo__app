4ea453762f1bfef8447d2450c265011ddf381721
manlikleo <olaotan104@gmail.com>
2021-02-21 11:53:42 +0300

    resposive nav bar and hero section

--- Todo_List/app.js
+++ Todo_List/app.js
@@ -1,20 +1,4 @@
-// Form 
-// variables
-// const form = document.getElementById('task-form');
-// const todoList = document.querySelector('.collection');
-// const taskInput = document.querySelector('#task');
-// const filter = document.querySelector('#filter');
-// const clearBtn = document.querySelector('.clearbtn');
-
-
-
-// loadEventListeners();
-
-// function loadEventListeners() {
-
 document.addEventListener('DOMContentLoaded', function() {
-
-    console.log("content loaded");
 
     // opening and closing navbar
     const openMenu = document.querySelector("#openmenu");
@@ -25,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     openAndClose()
 });
+
+
 
 
 function openAndClose(event) {
@@ -41,142 +27,36 @@ function openAndClose(event) {
 
 }
 
+export default class LocalStorage {
+
+    constructor() {
+        // getting task items from local storage or returns empty array if null 
+        this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    }
 
 
+    create(data) {
 
+        // getter token to generate a token for tasklist array 
+        data.token = this.token;
 
-// function getTasks(e) {
+        // creating task item  and setting to local storage
+        this.task.push(data);
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
 
-//     let tasks;
-//     if (localStorage.getItem('tasks') === null) {
-//         tasks = [];
-//     } else {
-//         tasks = JSON.parse(localStorage.getItem('tasks'));
-//     }
+    // this method will search for a task index for updating or editing
+    getIndexByToken(token) {
+        for (let i = 0; i < this.tasks.length; i++) {
+            if (this.tasks[i].token === token) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-//     tasks.forEach(function(task) {
-//         const li = document.createElement('li');
+    get token() {
+        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
 
-//         li.className = 'collection-item';
-
-//         li.appendChild(document.createTextNode(task));
-
-//         const removeBtn = document.createElement('a');
-//         removeBtn.className = 'remove-todo secondary-content';
-//         removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
-
-//         li.appendChild(removeBtn);
-
-//         todoList.appendChild(li);
-//     });
-
-// }
-
-// function addTask(e) {
-
-//     if (taskInput.value === '') {
-//         alert('Please input a task');
-//     } else {
-
-//         const li = document.createElement('li');
-
-//         li.className = 'collection-item';
-
-//         li.appendChild(document.createTextNode(taskInput.value));
-
-//         const removeBtn = document.createElement('a');
-//         removeBtn.className = 'remove-todo secondary-content';
-//         removeBtn.innerHTML = '<i class="fas fa-trash"></i>';
-
-//         li.appendChild(removeBtn);
-
-//         todoList.appendChild(li);
-
-//         storeTasks(taskInput.value);
-
-//         taskInput.value = ' ';
-
-//         e.preventDefault();
-
-//     }
-
-// }
-
-// function storeTasks(task) {
-
-//     let tasks;
-//     if (localStorage.getItem('tasks') === null) {
-//         tasks = [];
-//     } else {
-//         tasks = JSON.parse(localStorage.getItem('tasks'));
-//     }
-
-//     tasks.push(task);
-
-//     localStorage.setItem('tasks', JSON.stringify(tasks));
-// }
-
-
-// function removeTask(e) {
-//     if (e.target.parentElement.classList.contains('remove-todo')) {
-//         if (confirm('Are you Sure?')) {
-//             e.target.parentElement.parentElement.remove();
-//             // remove from Storage
-//             removeFromStorage(e.target.parentElement.parentElement);
-//         }
-//     }
-// }
-
-// // remove from local storage
-// function removeFromStorage(taskItem) {
-//     let tasks;
-//     if (localStorage.getItem('tasks') === null) {
-//         tasks = [];
-//     } else {
-//         tasks = JSON.parse(localStorage.getItem('tasks'));
-//     }
-
-//     tasks.forEach(function(task, index) {
-//         if (taskItem.textContent === task) {
-//             tasks.splice(index, 1);
-//         }
-//     });
-
-//     localStorage.setItem('tasks', JSON.stringify(tasks));
-// }
-
-// function ClearTask(e) {
-//     if (confirm('Are you Sure?')) {
-//         while (todoList.firstChild) todoList.removeChild(todoList.firstChild);
-//         alert('List cleared')
-
-//         // clear tasks from storage 
-
-//         ClearTasksFromStorage();
-
-
-//     } else {
-//         alert('Nothing to Delete')
-//     }
-
-// }
-
-// // function to clear from local storage 
-
-// function ClearTaskfromStorage() {
-//     localStorage.clear();
-// }
-
-// function filterTask(e) {
-//     const SearchTask = e.target.value.toLowerCase();
-
-//     document.querySelectorAll('.collection-item').forEach(function(task) {
-
-//         const item = task.firstChild.textContent;
-//         if (item.toLowerCase().indexOf(SearchTask) != -1) {
-//             task.style.display = 'block';
-//         } else {
-//             task.style.display = 'none';
-
-//         }
-//     });
+}
