cf80d1382278ded1f2d5c228ba43dbbb02228ebd
manlikleo <olaotan104@gmail.com>
2021-02-24 01:41:32 +0300

    populating taskwrapper using card template

--- Todo_List/app.js
+++ Todo_List/app.js
@@ -1,3 +1,13 @@
+// global scope
+
+const clearbtn = document.querySelector('#clearbtn');
+const taskForm = document.querySelector("#taskform");
+const editTask = document.querySelector("#edittask");
+const deleteTask = document.querySelector("#deletetask");
+const completeTask = document.querySelector("#checktask");
+
+let taskData = [];
+
 document.addEventListener('DOMContentLoaded', function() {
 
     // opening and closing navbar
@@ -8,14 +18,70 @@ document.addEventListener('DOMContentLoaded', function() {
     closeMenu.addEventListener("click", openAndClose, false);
 
     openAndClose()
+
+
+
+
+    // taskfrom eventlistener
+    taskForm.addEventListener('submit', e => {
+
+        // preventing default process
+        e.preventDefault();
+
+        // taskform input
+        let taskInput = taskForm['taskinput'].value;
+
+        // validating if input has data.
+        if (taskinput !== '') {
+
+            // input task inputdata
+            let data = {
+                id: new Date().getTime(),
+                taskInput,
+                isCompleted: false
+            };
+
+            // push data to the empty array taskData
+            taskData.push(data);
+
+            localStorage.setItem("taskData", JSON.stringify(data));
+
+            addTask(data)
+
+            e.target.reset();
+
+
+        }
+
+        taskForm['taskinput'].focus();
+
+    });
+
+
 });
 
+function addTask(data) {
+
+    const taskList = document.querySelector('#tasklistwrapper');
+    taskList.innerHTML += ` <div class="hero__appwrapper__tasklistwrapper__card">
+
+    <a class="hero__appwrapper__tasklistwrapper__taskcard" href="#">${data.taskInput}</a>
+
+    <div class="hero__appwrapper__tasklistwrapper__icons">
+        <i class="lni lni-checkbox"></i>
+        <i class="lni lni-pencil-alt"></i>
+        <i class="lni lni-trash"></i>
+    </div>
+
+</div>`;
 
 
+}
 
 function openAndClose(event) {
 
     const navLinks = document.getElementById('navlinks');
+
     if (typeof event !== "undefined" && typeof event.currentTarget !== "undefined") {
         if (event.currentTarget.id === "openmenu") {
             navLinks.classList.add('navbar_active');
@@ -23,40 +89,6 @@ function openAndClose(event) {
         if (event.currentTarget.id === "closemenu") {
             navLinks.classList.remove('navbar_active');
         }
-    }
-
-}
-
-export default class LocalStorage {
-
-    constructor() {
-        // getting task items from local storage or returns empty array if null 
-        this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    }
-
-
-    create(data) {
-
-        // getter token to generate a token for tasklist array 
-        data.token = this.token;
-
-        // creating task item  and setting to local storage
-        this.task.push(data);
-        localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    }
-
-    // this method will search for a task index for updating or editing
-    getIndexByToken(token) {
-        for (let i = 0; i < this.tasks.length; i++) {
-            if (this.tasks[i].token === token) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    get token() {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     }
 
 }
